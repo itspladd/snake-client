eec1f629b5ca97cff588bd05f790de5c2d6d0d97
Paul Ladd <vlendle@gmail.com>
2021-03-10 18:04:50 +0000

    Added basic input handling

--- play.js
+++ play.js
@@ -1,7 +1,29 @@
 const connect = require("./client");
 
-const stdin = process.stdin;
+const configInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleInput = key => {
+  switch (key) {
+    case "\u0003":
+      process.exit();
+      break;
+    
+  }
+};
+
+const input = configInput();
+
+input.on('data', key => {
+  handleInput(key);
+});
 
 console.log('Connecting...');
 
 connect();
+
